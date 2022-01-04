f74ca4db22cd7388a6dad2359a4dbe2b6d95ff80
Artem Bobkin <artem.bobkin@robusta.team>
2022-01-04 11:57:14 +0300

    firebase admin config from env variables

--- UserCommunicationService.Core/Services/Users/UsersService.cs
+++ UserCommunicationService.Core/Services/Users/UsersService.cs
@@ -1,6 +1,7 @@
 ﻿using FirebaseAdmin;
 using FirebaseAdmin.Auth;
 using Google.Apis.Auth.OAuth2;
+using Newtonsoft.Json;
 using UserCommunicationService.Core.Services.Users;
 using UserCommunicationService.Core.Services.Users.Models;
 
@@ -20,9 +21,49 @@ namespace UserCommunicationService.Core.Services
 
         public void Initialize()
         {
+            
+            // load from config file
+            var configFilePath = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+            if(string.IsNullOrEmpty(configFilePath))
+            {
+                FirebaseApp.Create(new AppOptions()
+                {
+                    Credential = GoogleCredential.FromFile(configFilePath)
+                });
+                return;
+            }
+
+
+
+            var type = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_TYPE");
+            var projectId = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_PROJECT_ID");
+            var privateKeyId = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID");
+            var privateKey = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY");
+            var clientEmail = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL");
+            var clientId = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_CLIENT_ID");
+            var authUri = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_AUTH_URI");
+            var tokenUri = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_TOKEN_URI");
+            var authProviderX509CertUrl = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_AUTH_PROVIDER_CERT_URL");
+            var clientX509CertUrl = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_CLIENT_CERT_URL");
+
+            var options = new GoogleCredentialsOptions() { 
+                Type = type,
+                ProjectId = projectId,
+                PrivateKey = privateKey,
+                PrivateKeyId = privateKeyId,
+                ClientEmail = clientEmail,
+                ClientId = clientId,
+                AuthUri = authUri,
+                TokenUri = tokenUri,
+                AuthProviderX509CertUrl = authProviderX509CertUrl,
+                ClientX509RertUrl = clientX509CertUrl,
+            };
+
+            var json = JsonConvert.SerializeObject(options);
+
             FirebaseApp.Create(new AppOptions()
             {
-                Credential = GoogleCredential.GetApplicationDefault()
+                Credential = GoogleCredential.FromJson(json),
             });
         }
 
@@ -53,6 +94,39 @@ namespace UserCommunicationService.Core.Services
                 messageDisplayName: record.DisplayName,
                 phoneNumber: record.PhoneNumber
             );
+        }
+
+        protected class GoogleCredentialsOptions
+        {
+            [JsonProperty(PropertyName = "type")]
+            public string Type { get; set; }
+
+            [JsonProperty(PropertyName = "project_id")]
+            public string ProjectId { get; set; }
+
+            [JsonProperty(PropertyName = "private_key_id")]
+            public string PrivateKeyId { get; set; }
+
+            [JsonProperty(PropertyName = "private_key")]
+            public string PrivateKey { get; set; }
+
+            [JsonProperty(PropertyName = "client_email")]
+            public string ClientEmail { get; set; }
+
+            [JsonProperty(PropertyName = "client_id")]
+            public string ClientId { get; set; }
+
+            [JsonProperty(PropertyName = "auth_uri")]
+            public string AuthUri { get; set; }
+
+            [JsonProperty(PropertyName = "TokenUri")]
+            public string TokenUri { get; set; }
+
+            [JsonProperty(PropertyName = "auth_provider_x509_cert_url")]
+            public string AuthProviderX509CertUrl { get; set; }
+
+            [JsonProperty(PropertyName = "client_x509_cert_url")]
+            public string ClientX509RertUrl { get; set; }
         }
     }
 }
